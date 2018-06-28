ee1af3f8364d70e436702ee86be4a5cb67d2abe1
pavolbincik <pavolbinck@gmail.com>
2018-06-28 18:04:07 +0200

    fix problem with uuid

--- src/createUuid.js
+++ src/createUuid.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const uuidv4 = require('uuid/v4');
 console.log('process.env.ID', process.env.ID);
-const id = process.env.ID || 0;
+const id = (process.env && process.env.ID) || 0;
 const path = `${__dirname}/uuid-${Number(id)}.js`;
 
 
 module.exports = function createUuid() {
   let uuid = uuidv4();
   if (fs.existsSync(path)) {
-    uuid = require('./uuid');
+    uuid = require(path);
   } else {
     fs.writeFile(path, `module.exports = '${uuid}';`, function (err) {
       if (err) throw err;
